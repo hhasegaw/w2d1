c49e2b19f82954db7bc9c14b39aa2b8df357a70b
Hanna <hanna@example.com>
2018-09-03 22:45:25 +0000

    first commit

--- task2.js
+++ task2.js
@@ -8,19 +8,20 @@ function getAndPrintHTML () {
     };
   
     https.get(requestOptions, function (response) {
-
-        // set encoding of received data to UTF-8
+    var buf = ""; 
+        // set encoding of red data to UTF-8
         response.setEncoding('utf8');
       
         // the callback is invoked when a `data` chunk is received
-        response.on('data', function (data) {
+        response.on('data', function (data) { 
+        buf += data 
           console.log('Chunk Received. Length:', data.length);
         });
       
         // the callback is invoked when all of the data has been received
         // (the `end` of the stream)
         response.on('end', function() {
-          console.log('Response stream complete.');
+          console.log('Response stream complete.' + buf);
         });
       
       });
